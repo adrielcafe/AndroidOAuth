d9fdb28ce1f08d885af9832772977cc204c3ae09
Adriel Café <me@adriel.cafe>
2016-07-07 11:37:05 -0300

    Refactoring

--- app/src/main/java/cafe/adriel/androidoauth/example/Credentials.java
+++ app/src/main/java/cafe/adriel/androidoauth/example/Credentials.java
@@ -1,10 +1,10 @@
 package cafe.adriel.androidoauth.example;
 
 public class Credentials {
-    public static final String GOOGLE_CLIENT_ID = "940891630461-uh38ng1a01tfkmu926g5f4el56h9hock.apps.googleusercontent.com";
-    public static final String GOOGLE_CLIENT_SECRET = "oxvq5lNSJvdM_zwZgc_appQV";
+    public static final String GOOGLE_CLIENT_ID = "REPLACE";
+    public static final String GOOGLE_CLIENT_SECRET = "REPLACE";
 
-    public static final String FACEBOOK_APP_ID = "1540807289473122";
-    public static final String FACEBOOK_APP_SECRET = "236e282277e8ee3150c447cf0307bcb3";
-    public static final String FACEBOOK_REDIRECT_URI = "http://demo.xarx.rocks/";
+    public static final String FACEBOOK_APP_ID = "REPLACE";
+    public static final String FACEBOOK_APP_SECRET = "REPLACE";
+    public static final String FACEBOOK_REDIRECT_URI = "REPLACE";
 }
